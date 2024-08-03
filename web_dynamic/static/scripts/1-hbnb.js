c811c7c59ea685efea5b6e97181b25fc6dad0a6a
Tanner Saint <tsaint2004@gmail.com>
2024-08-03 14:47:10 +0000

    added code to update the h4 amenity tag

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,2 +1,12 @@
+var checked_amenities = {};
+
 $(document).ready(function () {
-})
+  $('input[type=checkbox]').change(function () {
+    if (this.checked) {
+      checked_amenities[$(this.data('id'))] = ' ' + $(this).data('name');
+    } else {
+      delete checked_amenities[$(this).data('id')];
+    }
+    $('DIV.amenities h4').text(Object.values(checked_amenities));
+  });
+});
